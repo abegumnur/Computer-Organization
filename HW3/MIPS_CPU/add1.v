1315730d6fd149100ebb32fd751a9882414f4a26
abegumnur <96022054+abegumnur@users.noreply.github.com>
2023-04-13 07:11:07 +0300

    Delete HW3 directory

--- HW3/MIPS_CPU/add1.v
+++
@@ -1,8 +0,0 @@
-module add1 (a,b,ci,s,co); // full adder
-	input a, b, ci; // inputs: a, b, carry_in
-	output s, co; // outputs: sum, carry_out
-	
-	assign s = a ^ b ^ ci; // sum of inputs
-	assign co = a & b | b & ci | ci & a; // carry_out
-
-endmodule
